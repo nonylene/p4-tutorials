2c488b902d4e0cb4631f2f83af57d14ac32cea0c
nonylene <nonylene@gmail.com>
2019-05-23 20:11:10 +0900

    My setup script

--- vm/root-bootstrap.sh
+++ vm/root-bootstrap.sh
@@ -3,15 +3,14 @@
 # Print commands and exit on errors
 set -xe
 
-DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:webupd8team/atom
+# DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+# DEBIAN_FRONTEND=noninteractive sudo add-apt-repository -y ppa:webupd8team/atom
 
 apt-get update
 
 KERNEL=$(uname -r)
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 apt-get install -y --no-install-recommends \
-  atom \
   autoconf \
   automake \
   bison \
@@ -20,7 +19,6 @@ apt-get install -y --no-install-recommends \
   cmake \
   cpp \
   curl \
-  emacs24 \
   flex \
   git \
   libboost-dev \
@@ -47,7 +45,6 @@ apt-get install -y --no-install-recommends \
   libssl-dev \
   libtool \
   linux-headers-$KERNEL\
-  lubuntu-desktop \
   make \
   mktemp \
   pkg-config \
@@ -58,35 +55,32 @@ apt-get install -y --no-install-recommends \
   python-psutil \
   python-scapy \
   python-setuptools \
-  sublime-text-installer \
   tcpdump \
   unzip \
   vim \
-  wget \
-  xcscope-el \
-  xterm
+  wget
 
-useradd -m -d /home/p4 -s /bin/bash p4
-echo "p4:p4" | chpasswd
-echo "p4 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99_p4
-chmod 440 /etc/sudoers.d/99_p4
-usermod -aG vboxsf p4
+# useradd -m -d /home/p4 -s /bin/bash p4
+# echo "p4:p4" | chpasswd
+# echo "p4 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/99_p4
+# chmod 440 /etc/sudoers.d/99_p4
+# usermod -aG vboxsf p4
 
-cd /usr/share/lubuntu/wallpapers/
-cp /home/vagrant/p4-logo.png .
-rm lubuntu-default-wallpaper.png
-ln -s p4-logo.png lubuntu-default-wallpaper.png
-rm /home/vagrant/p4-logo.png
-cd /home/vagrant
-sed -i s@#background=@background=/usr/share/lubuntu/wallpapers/1604-lubuntu-default-wallpaper.png@ /etc/lightdm/lightdm-gtk-greeter.conf
+# cd /usr/share/lubuntu/wallpapers/
+# cp /home/vagrant/p4-logo.png .
+# rm lubuntu-default-wallpaper.png
+# ln -s p4-logo.png lubuntu-default-wallpaper.png
+# rm /home/vagrant/p4-logo.png
+# cd /home/vagrant
+# sed -i s@#background=@background=/usr/share/lubuntu/wallpapers/1604-lubuntu-default-wallpaper.png@ /etc/lightdm/lightdm-gtk-greeter.conf
 
 # Disable screensaver
-apt-get -y remove light-locker
+# apt-get -y remove light-locker
 
 # Automatically log into the P4 user
-cat << EOF | tee -a /etc/lightdm/lightdm.conf.d/10-lightdm.conf
-[SeatDefaults]
-autologin-user=p4
-autologin-user-timeout=0
-user-session=Lubuntu
-EOF
+# cat << EOF | tee -a /etc/lightdm/lightdm.conf.d/10-lightdm.conf
+# [SeatDefaults]
+# autologin-user=p4
+# autologin-user-timeout=0
+# user-session=Lubuntu
+# EOF
